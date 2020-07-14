32473e5623f5153f5cc42df9ee53e3d213c6e4d1
yudystriawan <yudistriawan@gmail.com>
2020-07-14 12:10:09 +0800

    create endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/*Buyers*/
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/*Categories*/
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/*Products*/
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+/*Sellers*/
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+/*Transactions*/
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+/*Users*/
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
